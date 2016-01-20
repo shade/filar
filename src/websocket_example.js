3537d1965428664fdaf9f47e5220225a01a4d8d1
jsjoe <joey6978@gmail.com>
2016-01-19 23:32:54 -0800

    Making websocket example

---
+++ src/websocket_example.js
@@ -0,0 +1,28 @@
+var socket	=	new WebSocket();
+
+var filar	=	new Filar().attachFile('.fileIcon',{
+	done:function(file){
+		socket.emit('fileHeader',file.header);
+	}
+});
+
+socket.on('headerAnswer',function(token){
+		file.token	=	token;
+		socket.emit('chunk',{
+			token:token,
+			number:0,
+			data:file.data[0]
+		});
+});
+
+socket.on('fileAnswer',function(res){
+	//needs error handling
+	if(res.error!=='none'||res.token==file.token)
+		return;
+	
+	//If that was the last chunk
+	if(file.number.length===++file.number){
+	
+	}
+		
+});
